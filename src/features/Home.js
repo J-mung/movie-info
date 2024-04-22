1b18121c9754814e6a450246bf584928f15eec61
TAEGYU JANG <xorb2298@gmail.com>
2024-04-23 00:22:41 +0900

    영화 api fetch

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,5 +1,32 @@
 import { MobileView } from 'react-device-detect';
+import { useQuery } from 'react-query';
+import { Text, useEffect } from 'react';
+
 const Home = () => {
+  const fetchMovies = async () => {
+    const response = await fetch(
+      'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json' +
+        '?key=' +
+        process.env.REACT_APP_API_KEY
+    );
+    const data = await response.json();
+
+    return data;
+  };
+  const { data, isLoading, error } = useQuery('movies', fetchMovies);
+
+  useEffect(() => {
+    console.log('data: ', data);
+  }, [data]);
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  }
+
   return (
     <MobileView>
       <div className="home">
